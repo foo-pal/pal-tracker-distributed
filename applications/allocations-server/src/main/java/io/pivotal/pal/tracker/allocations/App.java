c1651ad3636404b2475e1955877cb7ba5fe92fed
foo <foo@bar>
2017-07-19 16:47:05 -0600

    Add OAuth

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
@@ -18,6 +20,8 @@ import java.util.TimeZone;
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableWebSecurity
+@EnableResourceServer
 public class App {
 
     public static void main(String[] args) {
